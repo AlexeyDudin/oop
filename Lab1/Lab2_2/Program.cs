33e0a668ac39d529ed560fbe54b1eb787aad5a1e
Alexey Dudin <dudin.lesha@gmail.com>
2023-03-13 17:49:02 +0300

    сделал Lab2_2

---
+++ Lab1/Lab2_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab2_2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var inputStream = Console.In;
+            var outputStream = Console.Out;
+            while (true) 
+            {
+                Console.Clear();
+                Console.Write("Введите HTML-код (введите \'q\' для выхода из программы): ");
+                if (HtmlDecoder.DecodeHTML(inputStream, outputStream) == "q")
+                    break;
+                Console.WriteLine("Для продолжения нажмите любую клавишу");
+                Console.ReadKey();
+            }
+        }
+    }
+}
