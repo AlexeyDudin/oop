87b284eb145467e1aad9690f6f21669667f9e0fa
Alexey Dudin <dudin.lesha@gmail.com>
2023-03-13 20:26:03 +0300

    - start 2.4

---
+++ Lab1/Lab2_4/DictionaryWorker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace Lab2_4
+{
+    public static class DictionaryWorker
+    {
+        private static void FillEratosphen(Dictionary<int, bool> dictionary)
+        {
+            for (int i = 2; i < dictionary.Count; i++)
+            {
+                if (dictionary[i])
+                    SetValuesDisable(dictionary, i);
+            }
+        }
+
+        private static void SetValuesDisable(Dictionary<int, bool> dictionary, int i)
+        {
+            int multiplyer = 2;
+            while (multiplyer * i < (uint)dictionary.Count)
+            {
+                dictionary[multiplyer * i] = false;
+                multiplyer++;
+            }
+        }
+
+        public static Dictionary<int, bool> GeneratePrimeNumbersSet(int upperBound)
+        {
+            Dictionary<int, bool> result = FillDictionary(upperBound);
+            FillEratosphen(result);
+            return result;
+        }
+
+        private static Dictionary<int, bool> FillDictionary(int upperBound)
+        {
+            Dictionary<int, bool> result = new Dictionary<int, bool>();
+            for (int i = 1; i < upperBound; i++)
+            {
+                result.Add(i, true);
+            }
+            return result;
+        }
+
+        public static void Write(Dictionary<int, bool> result)
+        {
+            for (int i = 1; i < result.Count; i++)
+            {
+                if (result[i])
+                    Console.Write($"{i} ");
+            }
+        }
+    }
+}
---
+++ Lab1/Lab2_4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab2_4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<int, bool> result = DictionaryWorker.GeneratePrimeNumbersSet(Int32.Parse(Console.ReadLine()));
+            DictionaryWorker.Write(result);
+            Console.ReadKey();
+        }
+    }
+}
