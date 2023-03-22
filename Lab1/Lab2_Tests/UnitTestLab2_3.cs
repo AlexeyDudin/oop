ca3db72bdb9d07ccc057afec534b67ebcdc5e46f
Alexey Dudin <dudin.lesha@gmail.com>
2023-03-22 15:49:58 +0300

    - add don't save changes test

--- Lab1/Lab2_Tests/UnitTestLab2_3.cs
+++ Lab1/Lab2_Tests/UnitTestLab2_3.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
 using Lab2_3;
+using System.Runtime.Remoting.Messaging;
 
 namespace Lab2_Tests
 {
@@ -30,8 +31,8 @@ namespace Lab2_Tests
             consoleWorker.Run(library);
             Assert.AreEqual<string>(">Неизвестное слово \"blue\". Введите перевод или пустую строку для отказа.\r\n>Слово \"blue\" сохранено в словаре как \"синий\".\r\n>синий\r\n>В словарь были внесены изменения. Введите Y или y для сохранения перед выходом.\r\n", sw.ToString());
             string ethalonJSON = "{\"FirstWord\":\"blue\",\"SecondWord\":\"\\u0441\\u0438\\u043D\\u0438\\u0439\"}";
-            string recievedJSON = File.ReadAllText("library.json");
-            Assert.IsTrue(recievedJSON.Contains(ethalonJSON));
+            string recieveJSON = File.ReadAllText("library.json");
+            Assert.IsTrue(recieveJSON.Contains(ethalonJSON));
         }
 
         [TestMethod]
@@ -46,8 +47,23 @@ namespace Lab2_Tests
             Assert.AreEqual<string>(">Неизвестное слово \"blue\". Введите перевод или пустую строку для отказа.\r\n>Слово \"blue\" сохранено в словаре как \"синий\".\r\nСлово \"blue\" сохранено в словаре как \"голубой\".\r\n>blue\r\n>В словарь были внесены изменения. Введите Y или y для сохранения перед выходом.\r\n", sw.ToString());
             
             string ethalonJSON = "{\"FirstWord\":\"blue\",\"SecondWord\":\"\\u0441\\u0438\\u043D\\u0438\\u0439\"},{\"FirstWord\":\"blue\",\"SecondWord\":\"\\u0433\\u043E\\u043B\\u0443\\u0431\\u043E\\u0439\"}";
-            string recievedJSON = File.ReadAllText("library.json");
-            Assert.IsTrue(recievedJSON.Contains(ethalonJSON));
+            string recieveJSON = File.ReadAllText("library.json");
+            Assert.IsTrue(recieveJSON.Contains(ethalonJSON));
+        }
+
+        [TestMethod]
+        public void TestDictionaryDontSaveChanges()
+        {
+            TextReader tr = new StringReader("blue\n\n...");
+            StringWriter sw = new StringWriter();
+            var library = new Dictionary();
+            library.LoadLibrary("library.json");
+            var consoleWorker = new ConsoleWorker(tr, sw);
+            consoleWorker.Run(library);
+            string recieveJSON = File.ReadAllText("library.json");
+
+            Assert.IsFalse(recieveJSON.Contains("blue"));
+            Assert.AreEqual<string>(">Неизвестное слово \"blue\". Введите перевод или пустую строку для отказа.\r\n>Слово \"blue\" проигнорировано.\r\n>", sw.ToString());
         }
     }
 }
